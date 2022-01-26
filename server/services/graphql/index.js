51e590aacc5120aacd5e1022670b6413a63e82d0
Jose Ramirez <josemramirez81@outlook.com>
2022-01-26 13:45:11 -0500

    Setup express with Apollo

--- server/services/graphql/index.js
+++ server/services/graphql/index.js
@@ -1,7 +1,12 @@
 import { ApolloServer } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import Schema from './schema';
+import Resolver from './resolver';
 
-const executableSchema = makeExecutableSchema({});
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolver,
+});
 
 const server = new ApolloServer({
   schema: executableSchema,
