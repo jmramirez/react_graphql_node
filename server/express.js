d00740586a42cf8861e12646a1bb6bbd45df071c
Jose Ramirez <josemramirez81@outlook.com>
2022-01-26 13:43:05 -0500

    Setup logging using winston

--- server/express.js
+++ server/express.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import environment from './config/environment';
-import logger from 'morgan';
+import services from './services';
 
 const app = express();
 
-app.use(logger('dev', { skip: (req, res) => environment.env === 'test' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+(async () => {
+  try {
+    const servicesNames = Object.keys(services);
+    for (let i = 0; i < servicesNames.length; i += 1) {
+      const name = servicesNames[i];
+      if (name === 'graphql') {
+        await services[name].start();
+        services[name].applyMiddleware({ app });
+      } else {
+        console.log(name);
+        app.use(`/${name}`, services[name]);
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
