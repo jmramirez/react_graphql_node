181b7373f7889a073aeaddc594ba3354f7513fec
Jose Ramirez <josemramirez81@outlook.com>
2022-02-08 12:29:54 -0500

    Create relationship between Users and Posts

--- server/models/post.js
+++ server/models/post.js
@@ -2,11 +2,13 @@ import { Model, DataTypes } from 'sequelize';
 
 export default (sequelize) => {
   class Post extends Model {
-    static associate(models) {}
+    static associate(models) {
+      Post.belongsTo(models.User, { foreignKey: 'userId'})
+    }
   }
 
   Post.init(
-    { text: { type: DataTypes.TEXT } },
+    { text: { type: DataTypes.TEXT }, userId: { type: DataTypes.INTEGER } },
     { sequelize, modelName: 'Post' }
   );
 
--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ import { Model, DataTypes } from 'sequelize';
 export default (sequelize) => {
   class User extends Model {
     static associate(models) {
-
+      User.hasMany(models.Post, { as: 'Posts', foreignKey: 'userId'})
     }
   }
   User.init({
