872af6ab266e8d2e4ecf9e93d748324cd692d82c
Jose Ramirez <josemramirez81@outlook.com>
2022-02-08 14:41:09 -0500

    Associate Chat model with users

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ export default (sequelize) => {
   class User extends Model {
     static associate(models) {
       User.hasMany(models.Post, { as: 'Posts', foreignKey: 'userId'})
+      User.belongsToMany(models.Chat, { through: 'users_chats'})
     }
   }
   User.init({
