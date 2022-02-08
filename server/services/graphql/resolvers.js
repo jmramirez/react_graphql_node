4569c01c7f29af801d501a83d6b6780abab26f94
Jose Ramirez <josemramirez81@outlook.com>
2022-02-08 16:33:30 -0500

    Define resolvers to query chats and messages

--- server/services/graphql/resolvers.js
+++ server/services/graphql/resolvers.js
@@ -1,27 +1,8 @@
 import logger from '../../helpers/logger';
 
-let posts = [
-  {
-    id: 2,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar1.png',
-      username: 'Test User',
-    },
-  },
-  {
-    id: 1,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar2.png',
-      username: 'Test User 2',
-    },
-  },
-];
-
 export default function resolver() {
   const { db } = this;
-  const { Post, User } = db.models;
+  const { Post, User, Chat, Message } = db.models;
 
   const resolvers = {
     Post: {
@@ -29,10 +10,44 @@ export default function resolver() {
         return post.getUser();
       }
     },
+    Message: {
+      user(message, args, context) {
+        return message.getUser();
+      },
+      chat(message, args, context) {
+        return message.getChat();
+      }
+    },
+    Chat: {
+      messages(chat, args, context) {
+        return chat.getMessages({ order: [['id', 'ASC']]});
+      },
+      users(chat, args, context) {
+        return chat.getUsers();
+      }
+    },
     RootQuery: {
       posts(root, args, context) {
         return Post.findAll({ order: [['createdAt', 'DESC']] });
       },
+      chats(root, args, context) {
+        return User.findAll().then((users) => {
+          if(!users.length) {
+            return [];
+          }
+
+          const usersRow = users[0]
+
+          return Chat.findAll({
+            include: [{ model: User, required: true, through: { where: { userId: usersRow.id }}}, { model: Message, as: 'Messages' }]
+          })
+        })
+      },
+      chat(root, { chatId }, context) {
+        return Chat.findByPk(chatId, {
+          include: [{ model: User, required: true },{ model: Message, as: 'Messages'}]
+        })
+      }
     },
     RootMutation: {
       addPost(root, { post }, context) {
